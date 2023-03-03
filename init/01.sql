02a441c53e362264259525d8037fd47f9f33637a
ayushmanadhikari <unixparadoxfb@gmail.com>
2023-03-03 23:23:38 +0545

    mysql debezium connector up and running

--- init/01.sql
+++ init/01.sql
@@ -1,9 +1,12 @@
+GRANT ALL PRIVILEGES ON *.* TO 'docker'@'%' WITH GRANT OPTION;
+
 CREATE TABLE demo (
     id int, 
     name varchar(20)
-)
+);
 
 INSERT INTO demo VALUES 
 (1, 'ayusman'),
-(2, 'adhikari'),
-(3, 'raj');
+(2, 'adhikari');
+
+
