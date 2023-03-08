a2ef9f3d91897f452eba4acb066d1211e88b6d1c
ayushmanadhikari <unixparadoxfb@gmail.com>
2023-03-08 14:59:14 +0545

    readKafka update

--- readKafka.py
+++ readKafka.py
@@ -2,6 +2,7 @@ import os
 from pyspark.sql.functions import from_json
 from pyspark import SQLContext
 from pyspark.sql import SparkSession
+from pyspark.sql.avro.functions import from_avro, to_avro
 
 
 CONS_KAFKA_SERVER = "localhost:29092, localhost:29093"
@@ -15,9 +16,15 @@ spark.sparkContext.setLogLevel("WARN")
 #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2,za.co.absa:abris_2.12:5.0.0 --repositories https://packages.confluent.io/maven/ pyspark-shell'
 
 
-readingStreamDF = spark.readStream.format("kafka").option("kafka.bootstrap.servers", CONS_KAFKA_SERVER).option("subscribe", CONS_KAFKA_TOPIC).option("startingOffsets", "earliest").load()
+readingStreamDF = spark.readStream.format("kafka").option("kafka.bootstrap.servers", CONS_KAFKA_SERVER).\
+    option("subscribe", CONS_KAFKA_TOPIC).option("startingOffsets", "earliest").load().\
+    select(from_avro("value"))
 
 readingStreamDF.writeStream.format("console").outputMode("append").start().awaitTermination()
+
+
+
+
 
 
 
